c5e7b525e1285afc516fb64016d070d17fe02ae5
NMHS Computer Science <NMHSCompSci@cvsu.org>
2022-01-25 14:44:42 -0500

    initial on new branch teacher

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from ast import Num, Store
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
+import math
 
 
 print("hello")
@@ -8,4 +9,32 @@ print("hello")
 # number
 num = 2
 # string
-'2'
+string = '2'
+# list 
+list1 = [2, 3, '5']
+# tuple 
+tuple1 = (1, 2, 3)
+
+x_vals = list(range(5))
+print(x_vals)
+print(type(x_vals))
+
+y_vals = []
+
+for value in x_vals: 
+    # sin
+    # y_vals.append(math.sin(value))
+    # parabola (x ^ 2)
+    y_vals.append(value ** 2)
+
+names = (
+    ('Wyatt', 'Homola'), 
+    ('Ian', 'Kramer'), 
+    ('Sarah', 'Jane')
+)
+
+for name in names:
+    l_length = len(name[1]) 
+    print(name[0][0])
+    print(name[1][l_length - 1])
+
