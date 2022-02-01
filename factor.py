855534f45bda74bf44b445566fccaf6c869fdf25
stemnmhs <stemnmhs@cvsu.org>
2022-02-01 15:04:48 -0500

    factoring stuff

--- factor.py
+++ factor.py
@@ -2,6 +2,8 @@
     For eulerproject problem 3. 
     Efficient factoring algorithms are a rich, ancient, and active area of study.  Remember this exercise!
 '''
+# See https://stackoverflow.com/questions/8220801/how-to-use-timeit-module/29512249 on use of timeit
+import timeit
 
 
 def naive(n):
@@ -19,6 +21,14 @@ def naive(n):
 
 
 if __name__ == '__main__':
+    # Composite of several small factors, 2 * 3 * 5 * 6 * 71 * 23 * 17 * 101 * 51 * 9 * 77 * 1212
+    composite = 21618970829905680
+    # Product of the Mersenne primes 43112609 * 57885161
+    mersenne_prod = 2495580313095049
+    # The single large Newman-Shanks-Williams prime
+    large_prime = 63018038201
+    min(timeit.Timer(function).repeat)
+
     while True:
         x = int(input())
         print(naive(x))
